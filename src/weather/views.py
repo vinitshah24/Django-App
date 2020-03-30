3c143df0031d4e9847cb2cf1fdd2934d8fb12080
Vinit Shah <shahvinit2244@gmail.com>
2020-03-30 11:53:11 -0400

    restructure

--- src/weather/views.py
+++ src/weather/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 import requests
 from .models import City
 from .forms import CityForm
-
+import os
 # Create your views here.
 
 
 def index(request):
-    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={APP_ID}'
+    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + \
+        os.getenv('WEATHER_APP_KEY')
 
     # Saves form data directly to the table
     if request.method == 'POST':
